6826a7e11d8b796ec21f7ae89a5e84fc9a8c6b0c
Aritra Khatua <aritrakhatua20@gmail.com>
2025-05-26 10:03:01 +0530

    commit

--- src/main/java/org/aritra/accounts/Service/Impl/AccountServiceImpl.java
+++ src/main/java/org/aritra/accounts/Service/Impl/AccountServiceImpl.java
@@ -1,24 +1,45 @@
 package org.aritra.accounts.Service.Impl;
 
 import lombok.AllArgsConstructor;
+import org.aritra.accounts.Contant.AccountsConstants;
 import org.aritra.accounts.DTO.CustomerDto;
+import org.aritra.accounts.Entity.Account;
+import org.aritra.accounts.Entity.Customer;
+import org.aritra.accounts.Mapper.CustomerMapper;
 import org.aritra.accounts.Repository.AccountRepository;
 import org.aritra.accounts.Repository.CustomerRepository;
 import org.aritra.accounts.Service.IAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 @AllArgsConstructor
 public class AccountServiceImpl implements IAccountService {
-    @Autowired
+
+
     private AccountRepository accountRepository;
-    @Autowired
+
     private CustomerRepository customerRepository;
 
 
     @Override
     public void createAccount(CustomerDto customerDto) {
-
+        Customer customer= CustomerMapper.mapToCustomer(customerDto, new Customer()); // CustomerDto will be converted to CustomerObject
+       Customer savedCustomer= customerRepository.save(customer);
     }
+
+
+       private Account createNewAccount(Customer customer){ // accept Customer entity in method parameter
+        Account newAccount= new Account();
+        newAccount.setCustomeId(customer.getCustomerId());// populating or setting in NewAccount
+           long randomAccNumber = 1000000000L + new Random().nextInt(900000000); //seting random accountNumber
+
+
+           newAccount.setAccountNumber(randomAccNumber);
+           newAccount.setAccountType(AccountsConstants.SAVINGS);
+           newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+           return newAccount;
+       }
 }
